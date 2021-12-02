b550630900084840266964fb4ddcb907ce0ad3aa
Damiao.Junior <damiao.junior@softdesign.com.br>
2021-12-02 12:49:55 -0300

    feito

--- app/src/main/java/com/example/warrenchallenge/data/CallBack.kt
+++ app/src/main/java/com/example/warrenchallenge/data/CallBack.kt
@@ -3,7 +3,7 @@ package com.example.warrenchallenge.data
 import com.example.warrenchallenge.model.Token
 
 interface CallBack<T> {
-    fun onSuccessful(token: T): String
+    fun onSuccessful(token: T)
 
     fun onFailure(t: Throwable)
 }
